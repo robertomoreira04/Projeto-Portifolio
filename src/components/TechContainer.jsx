63e08cf768c85ab93a4cdc3887c0492fdc8e19b6
robertomoreira04 <robertodev04@gmail.com>
2024-07-24 14:21:42 -0300

    atualização

--- src/components/TechContainer.jsx
+++ src/components/TechContainer.jsx
@@ -7,15 +7,19 @@ import {
   DiSass,
 } from 'react-icons/di'
 
+import { SiTypescript, SiTailwindcss } from 'react-icons/si' 
+
 import '../styles/components/techcontainer.sass'
 
 const tech = [
   {id: 'html', name: "HTML5", icon: <DiHtml5 /> },
   {id: 'css', name: "CSS3", icon: <DiCss3 /> },
   {id: 'js', name: "JavaScript", icon: <DiJsBadge /> },
+  {id: 'ts', name: "Typescript", icon: <SiTypescript /> },
   {id: 'react', name: "React", icon: <DiReact/> },
   {id: 'node', name: "Node.js", icon: <DiNodejsSmall /> },
   {id: 'sass', name: "Sass", icon: <DiSass /> },
+  {id: 'tailwind', name: "Tailwind CSS", icon: <SiTailwindcss /> },
 ];
 
 const TechContainer = () => {
