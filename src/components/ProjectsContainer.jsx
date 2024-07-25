596626c21298ff4d4ca856c2c067979e790f2e97
robertomoreira04 <robertodev04@gmail.com>
2024-07-25 08:09:55 -0300

    .

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -4,7 +4,10 @@ const ProjectsContainer = () => {
     <h2>Projetos</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, culpa delectus? Quo impedit obcaecati quasi tenetur reprehenderit consequatur nostrum sint, facilis quae, vero iure architecto voluptate, nihil saepe pariatur tempora.
     </p>
-    <a href="#" className="btn">
+    <a 
+    href="https://github.com/robertomoreira04?tab=repositories" 
+    className="btn" 
+    target="_blank">
       Ver Projetos
     </a>
   </section>
